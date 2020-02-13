e650eb582c1209c05acd6a4bad955b6a28f325f6
sushant66 <snlkadam45@gmail.com>
2020-02-13 20:36:42 +0530

    Image to HSV

--- hsv_image.py
+++ hsv_image.py
@@ -3,7 +3,21 @@ import numpy as np
 
 img = cv2.imread('image.png')
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+print('Original Dimensions : ',img.shape)
+ 
+#scale_percent = 60 # percent of original size
+#width = int(img.shape[1] * scale_percent / 100)
+#height = int(img.shape[0] * scale_percent / 100)
+#dim = (width, height)
+# resize image
+resized = cv2.resize(img, (200, 200), interpolation = cv2.INTER_AREA)
+ 
+print('Resized Dimensions : ',resized.shape) 
+cv2.imshow('image', resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+hsv = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
 hue = hsv[:,:,0]
 sat = hsv[:,:,1]
 val = hsv[:,:,2]
