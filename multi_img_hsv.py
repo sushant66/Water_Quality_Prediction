38b27b1a33062cb5fc353a9c410876da6dbfa167
sushant66 <snlkadam45@gmail.com>
2020-02-14 22:44:24 +0530

    dataset

--- multi_img_hsv.py
+++ multi_img_hsv.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
+import pandas as pd
 
 img = []
-for i in range(0,2):    #change as per length of dataset
-	img.append(cv2.imread('test_img/image' + str(i) + '.png'))
+for i in range(0,14):    #change as per length of dataset
+	img.append(cv2.imread('images/image' + str(i) + '.jpg'))
+
 
 resized = []
 hue, sat, val = ([], [], [])
@@ -11,20 +13,19 @@ for i in range(len(img)):
 	resized.append(cv2.resize(img[i], (200, 200), interpolation = cv2.INTER_AREA))
  
 	hsv = cv2.cvtColor(resized[i], cv2.COLOR_BGR2HSV)
-	hue.append(hsv[:,:,0])
-	sat.append(hsv[:,:,1])
-	val.append(hsv[:,:,2])
+	hue.append(np.mean(hsv[:,:,0]))
+	sat.append(np.mean(hsv[:,:,1]))
+	val.append(np.mean(hsv[:,:,2]))
 
-for i in range(len(img)):
-	print('hue ' + str(np.mean(hue[i])), end = ' ')
-	print('sat ' + str(np.mean(sat[i])), end = ' ')
-	print('val ' + str(np.mean(val[i])), end = ' ')
-	print('\n')
+
+# for i in range(len(img)):
+# 	print('hue ' + str(np.mean(hue[i])), end = ' ')
+# 	print('sat ' + str(np.mean(sat[i])), end = ' ')
+# 	print('val ' + str(np.mean(val[i])), end = ' ')
 	
-
-
-	
-#cv2.imshow('image', img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
+# 	print('\n')
+out1 = [520,556,518,583,629,703,754,786,866,954,1009,1020,1020,1022]
+data = {'Hue': hue, 'Sat': sat, 'Val': val, 'Out': out1}
+df = pd.DataFrame(data)
+df.to_csv(r'dataset.csv')
+print(df)
